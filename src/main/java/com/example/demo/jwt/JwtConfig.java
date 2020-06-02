ba492ab7c4b5f3b6cdae56cb6627aaf61aae582e
blackfire7 <sahil.sharma1912@gmail.com>
2020-06-02 12:05:59 +0530

    Ended

---
+++ src/main/java/com/example/demo/jwt/JwtConfig.java
@@ -0,0 +1,39 @@
+//package com.example.demo.jwt;
+//
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//@ConfigurationProperties(prefix = "application.jwt")
+//public class JwtConfig {
+//
+//    private String secretKey;
+//    private String tokenPrefix;
+//    private int tokenExpirationAfterDays;
+//
+//    public JwtConfig() {
+//    }
+//
+//    public String getSecretKey() {
+//        return secretKey;
+//    }
+//
+//    public void setSecretKey(String secretKey) {
+//        this.secretKey = secretKey;
+//    }
+//
+//    public String getTokenPrefix() {
+//        return tokenPrefix;
+//    }
+//
+//    public void setTokenPrefix(String tokenPrefix) {
+//        this.tokenPrefix = tokenPrefix;
+//    }
+//
+//    public int getTokenExpirationAfterDays() {
+//        return tokenExpirationAfterDays;
+//    }
+//
+//    public void setTokenExpirationAfterDays(int tokenExpirationAfterDays) {
+//        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+//    }
+//}
